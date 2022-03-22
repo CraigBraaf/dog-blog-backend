e777aaf07000c2d7396a5c1312b7377c4974926c
Craig <braafcraig@gmail.com>
2022-03-22 09:01:22 +0200

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,12 @@ const Post = require("../models/Post");
 
 //CREATE POST
 router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
+  const newPost = new Post ({
+    title: req.body.title,
+    description: req.body.description,
+    photo: req.body.photo,
+    username: req.body.username,
+  })
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
